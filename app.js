4ec20ec4424264451787baedb794051e630ccdea
shiomi-takeshi <t.shiomi@eisansystem.jp>
2024-04-05 13:44:47 +0900

    first_commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+function Square() {
+  return <button className="square">X</button>;
+}
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <Square />
+  </StrictMode>
+);
